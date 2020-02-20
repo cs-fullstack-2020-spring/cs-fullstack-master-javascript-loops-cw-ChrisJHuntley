17c952794e45346b708a693b41836444fc6bb082
chris huntley <cjhuntley4@gmail.com>
2020-02-20 15:03:52 -0600

    almost finished

---
+++ index.js
@@ -0,0 +1,42 @@
+// Exercise 1********************************
+// for(let i=-3; i<=8; i++){
+//     console.log(i)
+// }
+// Exercise A********************************
+// let i=-10
+// while(i< 5){
+//     console.log(i)
+//     i++
+// }
+// Exercise 2*********************************
+// for(i=-5; i<=5; i++){
+
+// console.log(i)
+// console.log(i)
+// console.log(i)
+// }
+// Exercise B**********************************
+// let i=10
+// while(i>0){
+//     console.log(i)
+//     i--
+// }
+// Exercise 3***********************************
+// for(let i=-50; i<=50; i=i+2){
+//     console.log(i)
+// }
+// Exercise C***********************************
+// let name_array=[];
+// let no='q'
+// while(name_array != no){
+//     if(name_array==no){
+//         for(List= name_array; list.length< name_array; list++){
+//             console.log(list)
+//         }
+//     }
+// let name_array=prompt('Enter a word, or Enter q to Quit')
+// list=[name_array]
+// console.log(list)
+// }
+// Exercise 4*************************************
+
